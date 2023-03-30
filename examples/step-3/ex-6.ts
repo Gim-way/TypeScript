b7ea2a212a3e2733226345740150483e6199404c
Mikhail Timashov <bimbarr777@gmail.com>
2023-03-30 18:11:44 +0300

    Tuples

---
+++ examples/step-3/ex-6.ts
@@ -0,0 +1,8 @@
+const skill: [number, string] = [1, 'Dev']
+
+const [id, skillName] = skill
+
+const arrSpread: [number, string, ...boolean[]] = [1, 'test', true, false, true]
+
+
+/* Tuples - массив ограниченной длины, где каждый элемент типизированный  */
