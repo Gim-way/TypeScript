d45882596b37069bc9e6cc8601d319de71453a2f
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-02 09:54:02 +0300

    Readonly

---
+++ examples/step-3/ex-7.ts
@@ -0,0 +1,6 @@
+const skiEX7: readonly [number, string] = [1, 'Dev']
+
+const skiEX7String: ReadonlyArray<string> = ['Dev', 'DevOps']
+
+
+/* Зафиксировали длину массива, не можем модифицировать массив */
