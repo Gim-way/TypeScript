2ffe48fb501cd6e7572bc714396b374745a4f08f
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-03 21:32:27 +0300

    Implements

---
+++ examples/step-5/ex-6.ts
@@ -0,0 +1,30 @@
+interface ILogger {
+  log(...args): void
+
+  error(...args): void
+}
+
+class Looger implements ILogger {
+  log(...args: any[]): void {
+    console.log(...args)
+  }
+
+  async error(...args: any[]): Promise<void> {
+    // Кинуть во внешнюю систему
+    console.log(...args)
+  }
+}
+
+interface IPayable {
+  pay(paymentId: number): void
+
+  price?: number
+}
+
+class UserS5EX6 implements IPayable {
+  pay(paymentId: number | string): void {
+    ///
+  }
+}
+
+/* Implements */
