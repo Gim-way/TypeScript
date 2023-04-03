82f59e5c854f4c1f1105da294041c02adbe5a40b
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-03 14:04:30 +0300

    Types или Interfaces?

---
+++ examples/step-4/ex-5.ts
@@ -0,0 +1,18 @@
+interface UserS4EX5 {
+  name: string
+}
+
+interface UserS4EX5 {
+  age: number;
+}
+
+const userS4EX5: UserS4EX5 = {
+  name: 'as',
+  age: 33
+}
+
+/* Разниться между Type и Interface, мы с помощью Interface дополняем следующий интерфейс
+* C помощью interface можно дополнить Библиотеку */
+/* рекомендуют использовать interface, если работает с object */
+
+/* Преимущество Type "type = string | number" использовать для примитивных типов */
