eaf4b420540266b3ea000fe4281b4c8fd6d8d326
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-03 22:53:05 +0300

    Static Properties

---
+++ examples/step-5/ex-11.ts
@@ -0,0 +1,25 @@
+class UserService {
+  // static name: string = 'sdf';
+  private static db: any
+
+  static getUser(id: number) {
+    return UserService.db.findById(id)
+  }
+
+  constructor(id: number) {
+  }
+
+  create() {
+    UserService.db
+  }
+
+  static {
+    UserService.db = 'sdf'
+  }
+}
+
+UserService.getUser(1)
+const inst = new UserService(1)
+inst.create()
+
+/* Статические свойства */
