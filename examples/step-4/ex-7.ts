143f933a7ef38d59937efa945d3f45941e01fcfb
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-03 15:35:10 +0300

    Void

---
+++ examples/step-4/ex-7.ts
@@ -0,0 +1,33 @@
+function logIdS4EX7(id: string | number): void {
+  console.log(id)
+}
+
+const aS4EX7 = logIdS4EX7(1)
+
+function multiply(f: number, s?: number) {
+  if (!s) {
+    return f * f
+  }
+}
+
+type voidFunc = () => void;
+
+const f1: voidFunc = () => {
+
+}
+
+const f2: voidFunc = () => {
+  return true
+}
+
+const b = f2()
+
+const skillsS4EX7 = ['Dev', 'DevOps']
+
+const userS4EX7 = {
+  s: ['s']
+}
+
+skills.forEach((skillS4EX7) => userS4EX7.s.push(skillS4EX7))
+
+/* Void - функция ничего не возвращает  */
