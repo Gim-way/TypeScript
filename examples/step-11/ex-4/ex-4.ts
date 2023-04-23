6cb86fd0737335ca8197f4ca20ad99ef2d6e3d12
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-23 13:04:41 +0300

    Import и export

---
+++ examples/step-11/ex-4/ex-4.ts
@@ -0,0 +1,11 @@
+import run, { aS11EX4, type MyType2S11EX4 } from '../ex-4'
+import running from '../ex-4'
+import * as all from '../ex-4'
+import { TestS11EX4 as Cl } from '../ex-4'
+import { type MyTypeS11EX4 } from '../ex-4'
+
+running()
+run()
+new Cl()
+console.log(aS11EX4)
+console.log(all.aS11EX4)
