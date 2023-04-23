1067f2c19fd9d3bb0e6b25801a4c590e98b524ea
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-23 11:57:10 +0300

    Modularity on the backend

---
+++ examples/step-11/ex-1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.a = void 0;
+exports.a = 5;
+/* namespace - Обёртка, инкапсулирующий в себя какую-нибудь логику*/
+/* на фронте использование минимальное */ 
---
+++ examples/step-11/ex-3.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ex_1_js_1 = require("./ex-1.js");
+console.log(ex_1_js_1.a);
+/* IMPORT */ 
