86f5a41085d2d42abcfe53bf0e3ad997c9d95c7f
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-23 12:11:55 +0300

    Modularity on the frontend

--- examples/step-11/ex-1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.a = void 0;
-exports.a = 5;
-/* namespace - Обёртка, инкапсулирующий в себя какую-нибудь логику*/
-/* на фронте использование минимальное */ 
