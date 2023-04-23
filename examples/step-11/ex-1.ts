6d7ba42e2d7abb72af9443d4920042654bd32c01
Mikhail Timashov <bimbarr777@gmail.com>
2023-04-23 11:22:03 +0300

    Namespaces и reference

---
+++ examples/step-11/ex-1.ts
@@ -0,0 +1,10 @@
+namespace AS11EX1 {
+  export const a = 5;
+
+  export interface B {
+    c: number;
+  }
+}
+
+/* namespace - Обёртка, инкапсулирующий в себя какую-нибудь логику*/
+/* на фронте использование минимальное */
---
+++ examples/step-11/ex-2.ts
@@ -0,0 +1,2 @@
+// <reference path="./examples/step-11/ex-1.ts" />
+console.log(AS11EX1.a)
